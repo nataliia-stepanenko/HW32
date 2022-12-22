5a7a4605b42cbc56d07eecf249df020674d51468
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-12-22 13:47:52 +0200

    Added hw32

---
+++ build/index.js
@@ -0,0 +1,26 @@
+"use strict";
+const getDiff = (a, b) => {
+    const mainArray = [...a];
+    const arrayToCompare = [...b];
+    const diffArray = [];
+    for (let item of arrayToCompare) {
+        if (!mainArray.includes(item)) {
+            diffArray.push(item);
+        }
+        ;
+    }
+    ;
+    for (let item of mainArray) {
+        if (!arrayToCompare.includes(item)) {
+            diffArray.push(item);
+        }
+        ;
+    }
+    ;
+    let amount = diffArray.length;
+    return { "amount": amount, "array": diffArray };
+};
+console.log(getDiff('abc', 'abcd')); // { amount: 1, array: ['d'] }
+console.log(getDiff('abcd', 'cdfe')); // { amount: 4, array: ['a', 'b', 'f', 'e'] }
+console.log(getDiff('abc', 'wbbcc')); // { amount: 2, array: ['a', 'w'] }
+console.log(getDiff('abc', 'abc')); // { amount: 0, array: [] }
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+const getDiff = (a: string, b: string) => {
+    const mainArray: string[] = [...a];
+    const arrayToCompare: string[] = [...b];
+    const diffArray: string[] = [];
+
+    for (let item of arrayToCompare) {
+        if (!mainArray.includes(item)) {
+            diffArray.push(item);
+        };
+    };
+
+    for (let item of mainArray) {
+        if (!arrayToCompare.includes(item)) {
+            diffArray.push(item);
+        };
+    };
+
+    let amount: number = diffArray.length;
+    return {"amount": amount, "array": diffArray};
+}
+
+console.log(getDiff('abc', 'abcd')); // { amount: 1, array: ['d'] }
+console.log(getDiff('abcd', 'cdfe')); // { amount: 4, array: ['a', 'b', 'f', 'e'] }
+console.log(getDiff('abc', 'wbbcc')); // { amount: 2, array: ['a', 'w'] }
+console.log(getDiff('abc', 'abc')); // { amount: 0, array: [] }
